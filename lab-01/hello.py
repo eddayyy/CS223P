25e395aa1de6841f1445d27a0a2d0ef3bb48ef0d
eddayyy <eduardonunez@csu.fullerton.edu>
2022-10-24 14:07:41 -0700

    Test

---
+++ lab-01/hello.py
@@ -0,0 +1,10 @@
+# Name: Eduardo Nunez
+# Date: 08/25/2022
+# File Purpose: Multiple hello functions
+
+def helloworld():
+    return "Hello World!"
+
+def helloname(name):
+    return "Hello " + name + "!"
+
---
+++ lab-01/main.py
@@ -0,0 +1,8 @@
+# Name: Eduardo Nunez
+# Date: 08/25/2022
+# File Purpose: Hello World Main Driver Program Passes My Name Through hellonam() function
+from hello import * 
+
+print("*** TUFFY TITAN HELLO PROGRAM ***")
+print(helloworld())
+print(helloname("Eduardo Nunez"))
